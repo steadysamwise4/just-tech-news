3dc73e7ce04507ec16b158833ee39ca2b2b337b8
Sam Davenport <samueldavenport@att.net>
2021-12-13 16:24:18 -0600

    Comments table now exists, Comments are integrated and associated with Posts and Users

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
-// get all users
+// get all posts
 router.get('/', (req, res) => {
     console.log('========================');
     Post.findAll({
@@ -16,6 +16,14 @@ router.get('/', (req, res) => {
         order: [['created_at', 'DESC']],
         include: [
             {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
                 model: User,
                 attributes: ['username']
             }
@@ -28,7 +36,7 @@ router.get('/', (req, res) => {
     });
 });
 
-// get one user
+// get one post
 router.get('/:id', (req, res) => {
     Post.findOne({
         where: {
@@ -42,6 +50,14 @@ router.get('/:id', (req, res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username'] 
