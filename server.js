99244a166d4e2180ebf60ec0b2cebeb49c8dd5a6
Sam Davenport <samueldavenport@att.net>
2021-12-15 09:58:28 -0600

    user can now login and logout. session data is stored in the database. logout button added to the ui

--- server.js
+++ server.js
@@ -4,13 +4,28 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.MY_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // handlebars
 app.engine('handlebars', hbs.engine);
